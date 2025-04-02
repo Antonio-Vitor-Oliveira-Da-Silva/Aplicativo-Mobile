741ded404070d4566bb68586a2f6e17c3d4687a5
Antonio Vitor Oliveira Da Silva <vo48908@gmail.com>
2025-04-02 09:59:33 -0300

    Retorno da URL

--- services/api.ts
+++ services/api.ts
@@ -2,5 +2,5 @@ import axios from "axios";
 
 // http://192.168.1.151:3333 /create
 export const api = axios.create({
-    baseURL: "https://aplicativo-mobile-theta.vercel.app/"
+    baseURL: "http://192.168.1.151:3333"
 })
