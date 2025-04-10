2d7125811aedfab7147203babc39ed3a9cfdf6ac
Antonio Vitor Oliveira Da Silva <vo48908@gmail.com>
2025-04-10 19:27:52 -0300

    Alteração Mobile 1

--- services/api.ts
+++ services/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-// http://192.168.1.151:3333 /create
+// http://172.17.6.149:3333 /create
 export const api = axios.create({
-    baseURL: "http://192.168.1.151:3333"
+    baseURL: "http://localhost:3333"
 })
